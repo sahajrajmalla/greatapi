2824aca16800160db0a85e6a2dd18587b0888400
sahajrajmalla <mallasahajraj@gmail.com>
2022-06-28 15:44:40 +0545

    :recycle: take file path from config

--- greatapi/commands/startapp.py
+++ greatapi/commands/startapp.py
@@ -2,13 +2,12 @@ from __future__ import annotations
 
 from pathlib import Path
 
+from greatapi.config import ROOT_PACKAGE_PATH
 from greatapi.utils.management import copy_files_and_dirs
-
-ROOT_PACKAGE_PATH = Path(__file__).parent.parent
 
 
 def application_initialization(app_name: str) -> None:
-    base_copy_path = ROOT_PACKAGE_PATH.joinpath('templates', 'startapp')
+    base_copy_path = ROOT_PACKAGE_PATH.joinpath("templates", "startapp")
     base_paste_path = Path(app_name)
 
     copy_files_and_dirs(base_copy_path, base_paste_path, app_name)
--- greatapi/commands/startproject.py
+++ greatapi/commands/startproject.py
@@ -2,13 +2,12 @@ from __future__ import annotations
 
 from pathlib import Path
 
+from greatapi.config import ROOT_PACKAGE_PATH
 from greatapi.utils.management import copy_files_and_dirs
-
-ROOT_PACKAGE_PATH = Path(__file__).parent.parent
 
 
 def project_initialization(project_name: str) -> None:
-    base_copy_path = ROOT_PACKAGE_PATH.joinpath('templates', 'startproject')
-    base_paste_path = Path('.')
+    base_copy_path = ROOT_PACKAGE_PATH.joinpath("templates", "startproject")
+    base_paste_path = Path(".")
 
     copy_files_and_dirs(base_copy_path, base_paste_path, project_name)
