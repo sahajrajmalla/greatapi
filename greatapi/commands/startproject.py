c67dbe77fad8e5c333bf01923fe7579069a2fe5d
sahajrajmalla <mallasahajraj@gmail.com>
2022-06-28 13:05:45 +0545

    :sparkles: Added startapp command

--- greatapi/commands/startproject.py
+++ greatapi/commands/startproject.py
@@ -8,7 +8,7 @@ ROOT_PACKAGE_PATH = Path(__file__).parent.parent
 
 
 def project_initialization(project_name: str) -> None:
-    base_copy_path = ROOT_PACKAGE_PATH.joinpath('templates', 'startproject')
-    base_paste_path = Path(project_name)
+    base_copy_path = ROOT_PACKAGE_PATH.joinpath("templates", "startproject")
+    base_paste_path = Path(".")
 
-    copy_files_and_dirs(base_copy_path, base_paste_path)
+    copy_files_and_dirs(base_copy_path, base_paste_path, project_name)
--- greatapi/utils/management.py
+++ greatapi/utils/management.py
@@ -4,13 +4,13 @@ import shutil
 from pathlib import Path
 
 
-def copy_files_and_dirs(base_copy_path: Path, base_paste_path: Path) -> None:
+def copy_files_and_dirs(base_copy_path: Path, base_paste_path: Path, name: str) -> None:
     if not base_paste_path.exists():
         base_paste_path.mkdir(parents=False, exist_ok=False)
 
     for child in base_copy_path.iterdir():
         child_path = Path(child)
         if child_path.is_dir():
-            shutil.copytree(child, base_paste_path.joinpath(child.name))
+            shutil.copytree(child, base_paste_path.joinpath(name))
         else:
             shutil.copyfile(child, base_paste_path.joinpath(child.name))
