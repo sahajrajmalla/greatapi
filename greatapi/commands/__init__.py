e85b14cd7ab3e4cfce6cbd46817701e18c16a202
sahajrajmalla <mallasahajraj@gmail.com>
2022-06-28 13:08:58 +0545

    :recycle: precommit refactor

--- greatapi/commands/__init__.py
+++ greatapi/commands/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import typer
+
 from greatapi.commands.startapp import application_initialization
 from greatapi.commands.startproject import project_initialization
 
@@ -9,9 +10,9 @@ app = typer.Typer()
 
 @app.command()
 def cli(first_arg: str, second_arg: str) -> None:
-    if first_arg == "startproject":
+    if first_arg == 'startproject':
         project_initialization(second_arg)
-    elif first_arg == "startapp":
+    elif first_arg == 'startapp':
         application_initialization(second_arg)
     else:
-        typer.echo("Command not found!")
+        typer.echo('Command not found!')
--- greatapi/commands/startapp.py
+++ greatapi/commands/startapp.py
@@ -8,7 +8,7 @@ ROOT_PACKAGE_PATH = Path(__file__).parent.parent
 
 
 def application_initialization(app_name: str) -> None:
-    base_copy_path = ROOT_PACKAGE_PATH.joinpath("templates", "startapp")
+    base_copy_path = ROOT_PACKAGE_PATH.joinpath('templates', 'startapp')
     base_paste_path = Path(app_name)
 
     copy_files_and_dirs(base_copy_path, base_paste_path, app_name)
