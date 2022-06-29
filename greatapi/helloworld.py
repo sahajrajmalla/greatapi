dd905a3e60c1db3077b1fe07943bdac3510aede6
sahajrajmalla <mallasahajraj@gmail.com>
2022-06-29 06:53:28 +0545

    :fire: removed hello_world example file

--- greatapi/helloworld.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-
-
-def say_hello(name: str | None = None) -> str:
-    if name is None:
-        return 'Hello mewow!'
-    else:
-        return f'Hello, {name}!'
