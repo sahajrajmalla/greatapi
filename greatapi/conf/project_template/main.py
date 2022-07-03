9ffc0c0320dd98f392c28577262c3ffb490061f2
sahajrajmalla <mallasahajraj@gmail.com>
2022-07-03 20:29:08 +0545

    :hammer: added core cbv routing into main.py

--- greatapi/conf/project_template/main.py
+++ greatapi/conf/project_template/main.py
@@ -6,17 +6,10 @@ from fastapi.staticfiles import StaticFiles
 from greatapi.config import GREATAPI_ADMIN_STATIC_PATH
 from greatapi.db.database import engine
 from greatapi.db.models.user import Base
-from greatapi.admin.sites import router
+from greatapi.admin.sites import admin_router
 from greatapi.admin.sites import AdminSite
 from greatapi.utils.cbv import _cbv
-
-# from greatapi.routers import admin
-# from greatapi.routers import auth
-# from greatapi.routers import index
-# from greatapi.routers import user
-
-
-
+from greatapi.core import auth_router, user_router, test_auth_router
 
 app = FastAPI()
 
@@ -24,19 +17,19 @@ app.mount('/static', StaticFiles(directory=GREATAPI_ADMIN_STATIC_PATH), name='st
 
 Base.metadata.create_all(engine)
 
-# app.include_router(admin.router)
-# app.include_router(index.router)
-# app.include_router(user.router)
-# app.include_router(auth.router)
-
 
 class ClassSahaj:
     name = "sahaj"
 
 dummy_str = {"sahaj": ClassSahaj}
 
-admin_site = _cbv(router, dummy_str, AdminSite)
+admin_site = _cbv(admin_router, AdminSite, dummy_str)
 
 
 
-app.include_router(router)
+app.include_router(admin_router)
+app.include_router(auth_router)
+app.include_router(user_router)
+app.include_router(test_auth_router)
+
+
