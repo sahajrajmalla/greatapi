e6bf6d9aaab793641d1e985c210e0484b4548b0e
sahajrajmalla <mallasahajraj@gmail.com>
2022-07-06 14:57:08 +0545

    :recycle: refactor setup commands to accomodate .py-tpl template format

--- greatapi/commands/startapp.py
+++ greatapi/commands/startapp.py
@@ -3,11 +3,11 @@ from __future__ import annotations
 from pathlib import Path
 
 from greatapi.config import ROOT_PACKAGE_PATH
-from greatapi.utils.management import copy_files_and_dirs
+from greatapi.utils.management import setup_app_template
 
 
 def application_initialization(app_name: str) -> None:
     base_copy_path = ROOT_PACKAGE_PATH.joinpath('conf', 'app_template')
     base_paste_path = Path(app_name)
 
-    copy_files_and_dirs(base_copy_path, base_paste_path, app_name)
+    setup_app_template(base_copy_path, base_paste_path, app_name)
