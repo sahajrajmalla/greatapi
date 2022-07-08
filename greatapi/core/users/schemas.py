1544d5f6660af9e6f291eb671146b6e234eaecb2
sahajrajmalla <mallasahajraj@gmail.com>
2022-07-08 20:20:07 +0545

    :sparkles: added username and contact in user create and get routers

--- greatapi/core/users/schemas.py
+++ greatapi/core/users/schemas.py
@@ -1,20 +1,28 @@
 from __future__ import annotations
 
+from typing import Optional
+
 from pydantic import BaseModel
 
 
 class UserSchema(BaseModel):
+    id: Optional[int]
     name: str
     email: str
     password: str
+    username: str
+    contact_number: str
 
     class Config():
         orm_mode = True
 
 
 class ShowUserSchema(BaseModel):
+    id: Optional[int]
     name: str
     email: str
+    username: str
+    contact_number: str
 
     class Config():
         orm_mode = True
