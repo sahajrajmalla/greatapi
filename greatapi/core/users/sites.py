e8f1caa1c28914b0e6e740ec66bb559fde63064d
sahajrajmalla <mallasahajraj@gmail.com>
2022-07-08 19:42:16 +0545

    :hammer: changed from depends to SessionLocal

--- greatapi/core/users/sites.py
+++ greatapi/core/users/sites.py
@@ -1,15 +1,12 @@
 from __future__ import annotations
 
-from typing import List
-from typing import Tuple
-
-from fastapi import Depends
-from sqlalchemy.orm import Session
+from fastapi import status
 
 from greatapi.core.users.query import create_new_user
 from greatapi.core.users.query import get_user_by_id
-from greatapi.db.database import get_db
-from greatapi.schemas.user import UserType
+from greatapi.core.users.schemas import ShowUserSchema
+from greatapi.core.users.schemas import UserSchema
+from greatapi.db.database import SessionLocal
 from greatapi.utils.cbv import cbv
 from greatapi.utils.inferring_router import InferringRouter
 
@@ -18,12 +15,12 @@ user_router = InferringRouter(tags=['Users'])
 
 @cbv(user_router)
 class UserSite:
-    db: Session = Depends(get_db)
+    db = SessionLocal()
 
-    @user_router.post('/user', response_model=List[Tuple[int, str]])
-    def create_user(self, request: UserType) -> list[tuple[int, str]]:
+    @user_router.post('/user', response_model=UserSchema, status_code=status.HTTP_201_CREATED)
+    def create_user(self, request: UserSchema) -> UserSchema:
         return create_new_user(request, self.db)
 
-    @user_router.get('/user/{id}', response_model=List[Tuple[int, str]])
-    def get_user(self, id: int) -> list[tuple[int, str]]:
+    @user_router.get('/user/{id}', response_model=ShowUserSchema)
+    def get_user(self, id: int) -> ShowUserSchema:
         return get_user_by_id(id, self.db)
