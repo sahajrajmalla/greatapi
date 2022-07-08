f72530161246911d9420c01dc18060c51481c4e1
sahajrajmalla <mallasahajraj@gmail.com>
2022-07-08 20:14:39 +0545

    :hammer: auth restriction added in test auth

--- greatapi/core/test_auth/sites.py
+++ greatapi/core/test_auth/sites.py
@@ -17,9 +17,9 @@ test_auth_router = InferringRouter(tags=['Test Authentication'])
 
 @cbv(test_auth_router)
 class TestAuthSite:
-    current_user: User = Depends(get_current_user)
+
     db = SessionLocal()
 
     @test_auth_router.get('/test_auth_user/{id}', response_model=List[Tuple[int, str]])
-    def get_user(self, id: int) -> list[tuple[int, str]]:
+    def get_user(self, id: int, current_user: User = Depends(get_current_user)) -> list[tuple[int, str]]:
         return get_user_by_id(id, self.db)
