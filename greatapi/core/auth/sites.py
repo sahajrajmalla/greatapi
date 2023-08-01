991932770d41b85fa0ac61d8586542bf17397cdf
Sahaj Raj Malla <51092972+sahajrajmalla@users.noreply.github.com>
2023-08-01 09:44:22 +0545

    :sparkles: login_admin API enpoint added

--- greatapi/core/auth/sites.py
+++ greatapi/core/auth/sites.py
@@ -42,3 +42,29 @@ class AuthSite:
         access_token = create_access_token(data={'sub': user.email})
 
         return {'access_token': access_token, 'token_type': 'bearer'}
+
+    @auth_router.post('/admin_login')
+    def admin_login(self, request: OAuth2PasswordRequestForm = Depends()) -> dict[str, Any]:
+        user = self.db.query(User).filter(User.email == request.username).first()
+        plain_password = request.password
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail='Invalid Credentials',
+            )
+
+        if not Hash.verify(user.password, plain_password):
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail='Incorrect Password',
+            )
+
+        if not user.is_admin:  # Check if the user is an admin
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail='Access denied. Only admin users are allowed to login.',
+            )
+        # Generate a JWT token and return
+        access_token = create_access_token(data={'sub': user.email})
+
+        return {'access_token': access_token, 'token_type': 'bearer'}
